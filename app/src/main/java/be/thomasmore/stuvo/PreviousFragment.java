678c1ce2b66190e0c416abf1c42deb9b95055ba9
mnmpower <mnmmaarten@hotmail.com>
2019-11-14 16:13:51 +0100

    renamed framgents

--- app/src/main/java/be/thomasmore/stuvo/PreviousFragment.java
+++ app/src/main/java/be/thomasmore/stuvo/PreviousFragment.java
@@ -13,6 +13,6 @@ public class PreviousFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_previous2,container, false);
+        return inflater.inflate(R.layout.fragment_previous ,container, false);
     }
 }
