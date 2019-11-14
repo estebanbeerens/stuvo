e0ae955d4ac367a18039a1fe2a1d7f1e46319a9c
mnmpower <mnmmaarten@hotmail.com>
2019-11-14 15:57:27 +0100

    Fixed the screwed up draw navbar to working clean navbar!!!!

--- app/src/main/java/be/thomasmore/stuvo/MainActivity.java
+++ app/src/main/java/be/thomasmore/stuvo/MainActivity.java
@@ -1,17 +1,23 @@
 package be.thomasmore.stuvo;
 
-import android.content.Intent;
+//import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+//import com.google.android.material.floatingactionbutton.FloatingActionButton;
+//import com.google.android.material.snackbar.Snackbar;
 
-import android.view.View;
+import android.util.Log;
+import android.view.MenuItem;
+//import android.view.View;
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
+//import androidx.navigation.NavController;
+//import androidx.navigation.Navigation;
+//import androidx.navigation.ui.AppBarConfiguration;
+//import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -20,58 +26,92 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.Menu;
+//import android.view.Menu;
 
-import be.thomasmore.stuvo.ui.previous.PreviousFragment;
+//import be.thomasmore.stuvo.ui.previous.PreviousFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    private AppBarConfiguration mAppBarConfiguration;
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+
+
+        // this is the action on the mail button
+//        FloatingActionButton fab = findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+        drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_previous, R.id.nav_requested,
-                R.id.nav_new, R.id.nav_friends, R.id.nav_logout)
-                .setDrawerLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.nav_app_bar_open_drawer_description, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.getDrawerArrowDrawable().setColor(Color.WHITE);
+        toggle.syncState();
+
+        Log.e("666666", savedInstanceState+"");
+        if (savedInstanceState == null){
+            Log.e("666666", "inside, sethome"+"");
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
+
+
     }
 
+
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch (menuItem.getItemId()){
+            case R.id.nav_home:
+                Log.e("666666", "Home"+"");
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
+                break;
+            case R.id.nav_friends:
+                Log.e("666666", "Friends"+"");
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new FriendsFragment()).commit();
+                break;
+            case R.id.nav_new:
+                Log.e("666666", "New"+"");
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new NewFragment()).commit();
+                break;
+            case R.id.nav_previous:
+                Log.e("666666", "Previous"+"");
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new PreviousFragment()).commit();
+                break;
+            case R.id.nav_requested:
+                Log.e("666666", "Requested"+"");
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new RequestedFragment()).commit();
+                break;
+
+
+        }
+        drawer.closeDrawer(GravityCompat.START);
+
         return true;
     }
 
     @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
     }
-
-//    public void navToPrevious_fragment(View v) {
-//        Intent intent = new Intent(this, PreviousFragment.class);
-//        startActivity(intent);
-//    }
 }
---
+++ app/src/main/java/be/thomasmore/stuvo/PreviousFragment.java
@@ -0,0 +1,18 @@
+package be.thomasmore.stuvo;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class PreviousFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_previous2,container, false);
+    }
+}
