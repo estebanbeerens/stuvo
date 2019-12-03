21ded149ff1f6ce69388305572583fd184b33fc4
estebanbeerens <esteban.beerens@gmail.com>
2019-12-03 14:46:59 +0100

    Changed layout fragments

--- app/src/main/java/be/thomasmore/stuvo/Fragments/RequestedFragment.java
+++ app/src/main/java/be/thomasmore/stuvo/Fragments/RequestedFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -12,9 +13,18 @@ import be.thomasmore.stuvo.R;
 
 public class RequestedFragment extends Fragment {
 
+    private View RootView;
+    private int studentId = 0;
+
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_requested,container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        RootView = inflater.inflate(R.layout.fragment_requested, container, false);
+
+        Bundle bundle = getArguments();
+        studentId = bundle.getInt("studentId");
+
+        return RootView;
     }
 }
